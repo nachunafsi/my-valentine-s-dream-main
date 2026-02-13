82572696ab9d9b713087e110d9481decd6af6eea
nafsisnoushad@gmail.com <your@email.com>
2026-02-14 01:30:43 +0530

    Update deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/my-valentine-s-dream-main/",
+  base: "/nachunafsi-my-valentine-s-dream-main/",
   server: {
     host: "::",
     port: 8080,
