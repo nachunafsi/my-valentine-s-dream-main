6170589bb67efe369ccea0a936f0cf82fdf2ccb1
nafsisnoushad@gmail.com <your@email.com>
2026-02-14 13:51:02 +0530

    fix: adjust config for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/my-valentine-s-dream-main/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
